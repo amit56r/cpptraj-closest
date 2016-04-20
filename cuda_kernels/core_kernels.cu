1e8fb4b71c79fbbd7eb00708b44e240ded288a8b
amit56r <amit56r@gmail.com>
2016-04-20 14:34:41 -0600

    fix and adding synchthread

--- cuda_kernels/core_kernels.cu
+++ cuda_kernels/core_kernels.cu
@@ -12,7 +12,12 @@ __global__ void Action_noImage_GPU(double *D_,double *maskCenter,double *Solvent
   double Dist;
   int t2;
   int t4;
-  D_[bx] = maxD;
+
+  if(tx == 0 && bx == 0)
+  	D_[bx] = maxD;
+  __syncthreads();
+
+
   int sIndex =  bx*NAtoms*3 + tx*3;
 
   Dist = maskCenter[0] * SolventMols_[sIndex + 0] + maskCenter[1] * SolventMols_[sIndex + 1] + maskCenter[2] * SolventMols_[sIndex + 2];
--- cuda_kernels/setup_routines.cpp
+++ cuda_kernels/setup_routines.cpp
@@ -56,7 +56,11 @@ void Action_Closest::cuda_action(Frame& frmIn, double maxD, Matrix_3x3 ucell, Ma
 	//copying back the D__ into the right place
 	for(int sMol = 0; sMol < NMols; sMol++){
 		SolventMols_[sMol].D = D_[sMol];
+		printf('lhs = %f ; rhs = %f ',SolventMols_[sMol].D,D_[sMol]);
 	}
+
+	delete linear_Solvent;
+	delete D_;
 
 
 	//done
