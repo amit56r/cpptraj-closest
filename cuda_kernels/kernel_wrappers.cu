e311e9f7ac1a0c7a8487c1b03cc908ace3cc461f
amit56r <amit56r@gmail.com>
2016-04-20 13:45:28 -0600

    fix to kernel

--- cuda_kernels/kernel_wrappers.cu
+++ cuda_kernels/kernel_wrappers.cu
@@ -1,5 +1,5 @@
 
-
+#include <csdtio>
 
 // device kernel def
 __global__ void Action_noImage_GPU(double *D_,double *maskCenter,double *SolventMols_,double maxD, int Nmols , int NAtoms);
@@ -27,7 +27,12 @@ void Action_NoImage_Center(double *SolventMols_,double *D_, double maskCenter[3]
   cudaMemcpy(devI2Ptr,SolventMols_,NMols * NAtoms * 3 * sizeof(double ),cudaMemcpyHostToDevice);
   dim3 dimGrid0 = dim3(NMols,1);
   dim3 dimBlock0 = dim3(NAtoms,1);
+
+
+  mprintf("About to launch kernel.\n");
   Action_noImage_GPU<<<dimGrid0,dimBlock0>>>(devO1Ptr,devI1Ptr, devI2Ptr, maxD, NMols, NAtoms);
+  mprintf("Done with kernel CUDA \n");
+  
   cudaMemcpy(D_,devO1Ptr,NMols * sizeof(double ),cudaMemcpyDeviceToHost);
   cudaFree(devO1Ptr);
   cudaFree(devI1Ptr);
