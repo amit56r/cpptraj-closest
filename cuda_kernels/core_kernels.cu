cbe714b6ea49f4237e696dd82d8f7db0221d5e47
amit56r <amit56r@gmail.com>
2016-04-21 05:58:55 -0600

    naive min reduction has been added

--- cuda_kernels/core_kernels.cu
+++ cuda_kernels/core_kernels.cu
@@ -5,27 +5,42 @@
 
 __global__ void Action_noImage_GPU(double *D_,double *maskCenter,double *SolventMols_,double maxD, int Nmols , int NAtoms)
 {
-  int bx;
-  bx = blockIdx.x;
-  int tx;
-  tx = threadIdx.x;
-  double Dist;
+	int bx;
+	bx = blockIdx.x;
+	int tx;
+	tx = threadIdx.x;
+	double Dist;
 
-  if(tx == 0 && bx == 0)
-  	D_[bx] = maxD;
-  __syncthreads();
+	if(tx == 0 && bx == 0)
+		D_[bx] = maxD;
+	__syncthreads();
 
 
-  int sIndex =  bx*NAtoms*3 + tx*3;
+	int sIndex =  bx*NAtoms*3 + tx*3;
 
-  double x =  maskCenter[0] - SolventMols_[sIndex + 0];
-  double y = maskCenter[1] - SolventMols_[sIndex + 1];
-  double z =  maskCenter[2] - SolventMols_[sIndex + 2];
-  Dist = x*x + y*y + z*z;
+	double x =  maskCenter[0] - SolventMols_[sIndex + 0];
+	double y = maskCenter[1] - SolventMols_[sIndex + 1];
+	double z =  maskCenter[2] - SolventMols_[sIndex + 2];
+	//Dist = x*x + y*y + z*z;
+	SolventMols_[sIndex] = x*x + y*y + z*z;
 	//printf(" dist  =  %f\n", Dist);
-  if (Dist  < D_[bx]) 
-    D_[bx] = Dist;
 
-  if(tx == 0 && bx == 0 )
-	printf("end of kernel");
+	__syncthreads();
+
+	//first thread
+	//naive approach to a reduction algorithm
+	//this works if NAtoms is small other wise you need split
+	//and do some of log(n) parallel reduction 
+	int i;
+	if( tx ==0 )
+	{
+		for(i  = 0 ; i < NAtoms ; i++ ){
+			sIndex = bx*NAtoms*3 + i*3
+			if (SolventMols_[sIndex]  < D_[bx]) 
+				D_[bx] = SolventMols_[sIndex];
+		}
+	}
+
+	if(tx == 0 && bx == 0 )
+		printf("end of kernel");
 }
